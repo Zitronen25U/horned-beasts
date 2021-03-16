b451984582967d48184d2673eec940abb1cb4477
Brian Lemons <brian_lemons@hotmail.com>
2021-03-16 10:13:09 -0700

    fixing lab :(

--- src/hornedbeasts.js
+++ src/hornedbeasts.js
@@ -7,6 +7,14 @@ class HornedBeasts extends React.Component {
       <h2>TITLE</h2>
       <img id="beastOne" src="https://via.placeholder.com/150" alt="img1"></img>
       <p>Description</p>
+
+      <h2>TITLE</h2>
+      <img id="beastTwo" src="https://via.placeholder.com/150" alt="img2"></img>
+      <p>Description</p>
+
+      <h2>TITLE</h2>
+      <img id="beastThree" src="https://via.placeholder.com/150" alt="img3"></img>
+      <p>Description</p>
       </div>
     );
   }
