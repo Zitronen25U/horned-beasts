934b780b0b21ff6d8d8e3d19e04e1de5bf0fb45b
Brian Lemons <brian_lemons@hotmail.com>
2021-03-16 10:01:30 -0700

    fixing the lab

---
+++ src/hornedbeasts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+  render() {
+    return (
+      <div> 
+      <h2>TITLE</h2>
+      <img id="beastOne" src="https://via.placeholder.com/150" alt="img1"></img>
+      <p>Description</p>
+      </div>
+    );
+  }
+}
+
+export default HornedBeast;
