4c11e5a19f12f91ee962388279e0ca28a4e1e25d
Brian Lemons <brian_lemons@hotmail.com>
2021-03-16 17:16:58 -0700

    adding lab 2

--- src/hornedbeasts.js
+++ src/hornedbeasts.js
@@ -1,21 +1,35 @@
 import React from 'react';
+import data from './data.json';
+// import title from './data.json';
 
 class HornedBeasts extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      beastVotes: 0
+    }
+  };
+
+  voteForABeast = () => {
+    this.setState({beastVotes: this.state.beastVotes + 1});
+  }
+
   render() {
     return (
-      <div> 
-      <h2>TITLE</h2>
-      <img id="beastOne" src="https://via.placeholder.com/150" alt="img1"></img>
-      <p>Description</p>
+      <>
+      {data.map(value => 
+        <div> 
+        <h2>{value.title}</h2>
 
-      <h2>TITLE</h2>
-      <img id="beastTwo" src="https://via.placeholder.com/150" alt="img2"></img>
-      <p>Description</p>
+        <img onClick={this.voteForABeast} id={value.title} src= {value.image_url} alt={value.keyword} height="200" width="200"></img>
 
-      <h2>TITLE</h2>
-      <img id="beastThree" src="https://via.placeholder.com/150" alt="img3"></img>
-      <p>Description</p>
-      </div>
+        <p>{value.description}</p>
+        <div>👍</div>
+        </div>
+        )
+      } 
+      </>
     );
   }
 }
